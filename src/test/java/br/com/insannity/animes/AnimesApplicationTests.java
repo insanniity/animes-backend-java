3a78ec1969bbe4b1c33000a0adbce12c84a87912
Cleydson <32269793+insanniity@users.noreply.github.com>
2022-01-10 23:14:35 -0300

    Project Start

---
+++ src/test/java/br/com/insannity/animes/AnimesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.insannity.animes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
